118c29bcc08f0f9650e1e90555a30ac92da91dba
mateohervas <mateohervas96@gmail.com>
2021-03-20 16:12:38 -0500

    add character entity mapper

--- app/src/test/java/com/shadows/mystarwarswiki/data/mappers/CharacterEntityMapperTest.kt
+++ app/src/test/java/com/shadows/mystarwarswiki/data/mappers/CharacterEntityMapperTest.kt
@@ -1,20 +1,33 @@
 package com.shadows.mystarwarswiki.data.mappers
 
+import com.shadows.core.models.local.StarWarsCharacter
+import com.shadows.mystarwarswiki.data.models.CharacterEntity
+import io.mockk.mockk
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
+
+@RunWith(JUnit4::class)
 class CharacterEntityMapperTest {
 
     @Test
-    fun toStarWarsCharacter() {
+    fun `map StarWarsCharacter to CharacterEntity`() {
+        val starWarsCharacter = StarWarsCharacter(emptyList(),"","","", emptyList(),"","","")
+        assertNotNull(starWarsCharacter.toCharacterEntity())
     }
 
     @Test
-    fun toCharacterEntity() {
+    fun `map CharacterEntity to StarWarsCharacter`() {
+        val characterEntity = CharacterEntity("","","","", emptyList(),"","","")
+        assertNotNull(characterEntity.toStarWarsCharacter())
     }
 
     @Test
-    fun toStarWarsCharacterList() {
+    fun `CharacterEntity list to StarWarsCharacter List`() {
+        val list = listOf(CharacterEntity("","","","", emptyList(),"","",""))
+        assertNotNull(list.toStarWarsCharacterList())
     }
 }
